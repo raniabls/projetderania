008c5d10077f12f995edca4a3d061efed812c031
raniabls <148482763+raniabls@users.noreply.github.com>
2023-10-31 13:50:30 +0100

    Add files via upload

---
+++ todolistprojetpaw/todolist.js
@@ -0,0 +1,51 @@
+const date = document.getElementById("date");
+const day  = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+
+const today = new Date();
+
+const weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const allMonths = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+date.innerHTML = (today.getDate()<10?"0":"") + today.getDate();
+day.innerHTML = weekDays[today.getDay()];
+month.innerHTML = allMonths[today.getMonth()];
+year.innerHTML = today.getFullYear();
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addtask(){
+    if(inputBox.value === '')
+        alert("You Must Write Something!");
+    
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);   
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } 
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
